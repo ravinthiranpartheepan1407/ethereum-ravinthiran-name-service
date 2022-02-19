287905031ca65d0fd134f20555b9b86fbb1c6d8a
Suren Partheepan <ravinthiranpartheepan1407@gmail.com>
2022-02-19 10:34:39 -0800

    ERNS

---
+++ pages/Header.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+function Header(){
+  return(
+    <ul className="flex bg-gradient-to-l from-indigo-500 to-bg-purple-400 p-8 text-white">
+  <li className="flex-1 mr-2">
+    <Link className="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="https://eth-gas-tracker-xi.vercel.app/">Gas Tracker</Link>
+  </li>
+  <li className="flex-1 mr-2">
+    <Link className="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="https://minter-app-ravinthiranpartheepan1407.vercel.app/">DAO</Link>
+  </li>
+  <li className="flex-1 mr-2">
+    <Link className="text-center bg-sky-400 block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="https://dapp-white-list-ravi.vercel.app/">WhiteList</Link>
+  </li>
+  <li className="flex-1 mr-2">
+    <Link className="text-center bg-sky-400 block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="#">ICO</Link>
+  </li>
+  <li className="text-center flex-1">
+    <Link className="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="#">DeFi</Link>
+  </li>
+  <li className="text-center flex-1">
+    <Link className="text-center block rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="/">Home</Link>
+  </li>
+
+</ul>
+  )
+}
+
+export default Header;
